79fdd1e579eead9e60ac3b505a0cadb2ed3583c5
Alisue <lambdalisue@gmail.com>
2024-02-04 05:57:14 +0900

    Adjusting maze size to fit the window

--- denops/denops-maze/main.ts
+++ denops/denops-maze/main.ts
@@ -1,13 +1,21 @@
 import type { Denops } from "https://deno.land/x/denops_std@v6.0.0/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v6.0.0/function/mod.ts";
 import { Maze } from "https://deno.land/x/maze_generator@v0.4.0/mod.js";
 
 export function main(denops: Denops): void {
   denops.dispatcher = {
     async maze() {
-      const maze = new Maze({}).generate();
+      await denops.cmd("enew");
+
+      const winWidth = await fn.winwidth(denops, 0);
+      const winHeight = await fn.winheight(denops, 0);
+      const maze = new Maze({
+        xSize: winWidth / 3,
+        ySize: winHeight / 3,
+      }).generate();
       const content = maze.getString();
 
-      await denops.call("setline", 1, content.split(/\r?\n/g));
+      await fn.setline(denops, 1, content.split(/\r?\n/g));
     },
   };
 }
