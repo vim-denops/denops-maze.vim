5161ede43a9ddb1b9dcdb13ad99003a85ed3f860
Alisue <lambdalisue@gmail.com>
2024-02-04 05:44:56 +0900

    Reduce the number of RPC calls

--- denops/denops-maze/main.ts
+++ denops/denops-maze/main.ts
@@ -1,4 +1,8 @@
 import type { Denops } from "https://deno.land/x/denops_std@v6.0.0/mod.ts";
+import {
+  batch,
+  collect,
+} from "https://deno.land/x/denops_std@v6.0.0/batch/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v6.0.0/buffer/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.0.0/function/mod.ts";
 import * as op from "https://deno.land/x/denops_std@v6.0.0/option/mod.ts";
@@ -9,19 +13,22 @@ export function main(denops: Denops): void {
     async maze() {
       const { bufnr, winnr } = await buffer.open(denops, "maze://");
 
-      const winWidth = await fn.winwidth(denops, winnr);
-      const winHeight = await fn.winheight(denops, winnr);
+      const [winWidth, winHeight] = await collect(denops, (denops) => [
+        fn.winwidth(denops, winnr),
+        fn.winheight(denops, winnr),
+      ]);
       const maze = new Maze({
         xSize: winWidth / 3,
         ySize: winHeight / 3,
       }).generate();
       const content = maze.getString();
 
-      await buffer.replace(denops, bufnr, content.split(/\r?\n/g));
-      await buffer.concrete(denops, bufnr);
-
-      await op.bufhidden.setLocal(denops, "wipe");
-      await op.modifiable.setLocal(denops, false);
+      await batch(denops, async (denops) => {
+        await buffer.replace(denops, bufnr, content.split(/\r?\n/g));
+        await buffer.concrete(denops, bufnr);
+        await op.bufhidden.setLocal(denops, "wipe");
+        await op.modifiable.setLocal(denops, false);
+      });
     },
   };
 }
